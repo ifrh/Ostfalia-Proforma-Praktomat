f017436827a79f1e84160e315461cd958601ad17
Karin Borm <k.borm@ostfalia.de>
2019-11-06 11:23:20 +0100

    cleanup database initialisation script

--- init_database.sh
+++ init_database.sh
@@ -10,22 +10,25 @@ if [ -e "$HOME/.DATABASE_INITIALISED" ];  then
 #    echo "Initialise Database"
 fi
 
-echo "syncing database"
+# echo "syncing database"
 # OLD python3 ./src/manage-docker.py syncdb --noinput --migrate
 # python3 ./src/manage-docker.py migrate --noinput
 #if [ $? -ne 0 ]; then 
 #    exit 1 
 #fi
 
+# clear python cache since there can be old files that confuse migrations
+echo "clean python cache"
+py3clean .
+
 # update tables in case of a modified or added checker
 echo "migrate schema"
 # do not use exit here!
-# OLD python3 ./src/manage-docker.py schemamigration checker --auto
 python3 ./src/manage-docker.py makemigrations --noinput
+
 # use initial in order to create initial migrations file
 #python ./src/manage.py schemamigration checker --initial
 echo "migrate checker"
-#OLD python3 ./src/manage-docker.py migrate checker || exit
 python3 ./src/manage-docker.py migrate || exit
 
 echo "create users"
