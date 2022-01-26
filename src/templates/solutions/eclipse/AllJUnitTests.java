51bbbc39ca37b1e756fe75876912ee06cf0f7243
Karin Borm <k.borm@ostfalia.de>
2022-01-26 06:47:26 +0100

    remove templates for UI

--- src/templates/solutions/eclipse/AllJUnitTests.java
+++
@@ -1,7 +0,0 @@
-import org.junit.runners.Suite;
-import org.junit.runner.RunWith;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ {% for testclass in testclasses  %} {{testclass}}.class, {% endfor %} })
-public class AllJUnitTests {
-}
