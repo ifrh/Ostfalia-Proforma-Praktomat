2714880e2342c2a3a5398ade4970f86a5e1ae61d
Karin Borm <k.borm@ostfalia.de>
2022-11-16 09:59:13 +0100

    update script

--- remove_migrations.sh
+++ remove_migrations.sh
@@ -1,3 +1,7 @@
+# remove database build files 
+
+# The migration can be build automatically on base of the source code. 
+
 sudo rm -rf src/accounts/migrations
 sudo rm -rf src/tasks/migrations
 sudo rm -rf src/checker/migrations
