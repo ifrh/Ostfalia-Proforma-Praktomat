21162700a414a6c1d175c33008ef7bb03202a82a
Karin Borm <k.borm@ostfalia.de>
2025-02-12 11:25:12 +0100

    change log options

--- src/settings/docker.py
+++ src/settings/docker.py
@@ -237,8 +237,8 @@ LOGGING = {
         },
         'utilities': {
             'handlers': ['console'],
-#            'level': 'INFO',  # change debug level as appropiate
-            'level': 'DEBUG',  # change debug level as appropiate
+            'level': 'INFO',  # change debug level as appropiate
+#            'level': 'DEBUG',  # change debug level as appropiate
             'maxBytes': 1024 * 1024 * 15,  # 15MB
             'backupCount': 10,  # keep 10 historical versions
         },
